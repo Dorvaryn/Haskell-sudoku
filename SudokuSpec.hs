4942c6eb903e9da2445c6b158226dff0b90a0d50
Dorvaryn <ben@novoda.com>
2013-11-23 15:15:21 +0000

    Test solving

--- SudokuSpec.hs
+++ SudokuSpec.hs
@@ -10,7 +10,7 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "sudoku" $ do
+    describe "base utils" $ do
         it "rows is reversible" $ do
             property $ reversibleForMatrix rows
         it "cols is reversible" $ do
@@ -21,6 +21,16 @@ spec = do
             reversibleForMatrix boxs easy `shouldBe` True
             reversibleForMatrix boxs try `shouldBe` True
             -- TODO: add an Arbitrary instance to generate valid matrixs : property $ reversibleForMatrix boxs
+    describe "sudoku" $ do
+        it "solve easy" $ do
+            solve easy `shouldBe` resultEasy
+        it "solveFast easy" $ do
+            solveFast easy `shouldBe` resultEasy
+        it "solveFast try" $ do
+            solveFast try `shouldBe` resultTry
 
 reversibleForMatrix :: (Grid -> Grid) -> Grid -> Bool
 reversibleForMatrix func matrix = (func $ func matrix) == matrix
+
+resultEasy = [["249571638","861432975","573986142","725698413","698143257","314725869","937814526","152369784","486257391"]]
+resultTry = [["295743861","431865927","876192543","387459216","612387495","549216738","763524189","928671354","154938672"]]
